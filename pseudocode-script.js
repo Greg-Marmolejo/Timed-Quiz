69ea74b309364ad5dc8bb80b0938f3f122c6782f
Greg Marmolejo <greg_marmolejo@yahoo.com>
2019-12-15 21:40:45 -0600

    added prompt to start quiz.  added commands to commit score and high score to local storage.

---
+++ pseudocode-script.js
@@ -0,0 +1,53 @@
+// create an array with 10 question objects. See 33-QuestionGame (Javascript)
+//Each object in the array should be structured as follows: 
+        //      { q: "QUESTION", a: "ANSWER" }
+        var questions = [
+            { q: "Inside which HTML element do we put the JavaScript?", chA:"script" chB:"javascript" chC:"js" a: "A" },
+            { q: "There are 365 days in a year.", a: "t" },
+            { q: "There are 42 ounces in a pound.", a: "f" },
+            { q: "The Declaration of Independence was created in 1745.", a: "f" },
+            { q: "Bananas are vegetables.", a: "f" }
+        ];
+   // We start the game with a score of 0.
+   var score = 0;
+
+   // Loop over every question object
+   for (var i = 0; i < questions.length; i++) {
+     // Display current question to user and ask OK/Cancel
+     var answer = confirm(questions[i].q);
+
+     // Compare answers
+     if ((answer === true && questions[i].a === "t") ||
+       (answer === false && questions[i].a === "f")) {
+       // Increase score
+       score++;
+       alert("Correct!");
+     }
+     else {
+       alert("Wrong!");
+     }
+   }
+
+   // Show total at end
+   alert("You got " + score + "/" + questions.length);
+   
+// Function to show question.  Uordered list similar to 18-Ins_Event_Delegation for multiple choice.
+   var listEl = document.querySelector("#grocery-list");
+   var shoppingCartEl = document.querySelector("#shopping-cart");
+   var groceries = ["Bananas", "Apples", "Oranges", "Grapes", "Blueberries"];
+   
+   listEl.addEventListener("click", function(event) {
+     event.preventDefault();
+     if(event.target.matches("button")) {
+       var item = document.createElement("div");
+       item.textContent = groceries[event.target.parentElement.id];
+       shoppingCartEl.append(item);
+     }
+   });
+// This should start the timer.
+
+// Function to compare answer to correct answer (triggered by click event)
+
+// Function to keep score (accounting for speed of answers as well as correctness)
+
+// Function for local storage.  See 20- Ins_Local-Storage-Counter.
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var pos = 0;
+confirm("Would you like to start the quiz?"); {
+
+var qNum = 0;
 var correct = 0;
 var test, test_status, question, choice, choices, chA, chB, chC;
 
+// create an array with 10 question objects.  See 33-QuestionGame (Javascript)
 var questions = [
     ["Inside which HTML element do we put the JavaScript?", "script", "javascript", "js", "A"],
     ["Where is the correct place to insert a JavaScript??", "The head section", "Both the head section and the body section", "The body section", "C"],
@@ -21,20 +24,17 @@ var questions = [
 
       function renderQuestion(){
         test = get("test");
-        if(pos >= questions.length){
+        if(qNum >= questions.length){
           test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
           get("test_status").innerHTML = "Test completed";
-          // resets the variable to allow users to restart the test
-          pos = 0;
-          correct = 0;
           // stops rest of renderQuestion function running when test is completed
           return false;
         }
-        get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
-        question = questions[pos][0];
-        chA = questions[pos][1];
-        chB = questions[pos][2];
-        chC = questions[pos][3];
+        get("test_status").innerHTML = "Question "+(qNum+1)+" of "+questions.length;
+        question = questions[qNum][0];
+        chA = questions[qNum][1];
+        chB = questions[qNum][2];
+        chC = questions[qNum][3];
         test.innerHTML = "<h3>"+question+"</h3>";
         // the += appends to the data we started on the line above
         test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
@@ -52,14 +52,25 @@ var questions = [
           }
         }
         // checks if answer matches the correct choice
-        if(choice == questions[pos][4]){
+        if(choice == questions[qNum][4]){
           //each time there is a correct answer this value increases
           correct++;
         }
         // changes position of which character user is on
-        pos++;
+        qNum++;
         // then the renderQuestion function runs again to go to next question
         renderQuestion();
     }
 
     window.addEventListener("load", renderQuestion, false);
+
+    // Sets the number answered correct to localstorage.
+    localStorage.setItem("score", correct);
+
+    // Compares the highscore to the number answered correct in this game.
+    var storagedHighScore = localStorage.getItem("highscore");
+if (storagedHighScore  || score > parseInt(storagedHighScore)) {
+  localStorage.setItem("highscore", score);
+}
+    // var correct = localStorage.getItem("score");
+  };

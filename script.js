2fd54de433eb93c323a4ed59d2fda16908eaf934
Greg Marmolejo <greg_marmolejo@yahoo.com>
2019-12-17 00:14:40 -0600

    changed timer to 15 seconds.  still needs button start functionality and another for-loop to prompt for additional attempts if first answer is wrong.

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-window.addEventListener("load", renderQuestion, false);
-
-console.log("Hello World")
-
-//records the time the quiz starts
-var start = Date.now();
-var highscore = 0;
-var highscore = localStorage.getItem("highscore");
 var qNum = 0;
-var correct = 0;
 var test, test_status, question, choice, choices, chA, chB, chC;
+var score = 25;
+var correct = 0;
+var pointsDeducted = 0
+var secondsRemaining = 5;
+var interval;
+var highscore = localStorage.getItem("highscore");
 
-// create an array with 10 question objects.  See 33-QuestionGame (Javascript)
+// create an array with 5 question objects.  5 Bonus Questions if anybody really wants
 var questions = [
   ["Inside which HTML element do we put the JavaScript?", "script", "javascript", "js", "A"],
-  ["Where is the correct place to insert a JavaScript??", "The head section", "Both the head section and the body section", "The body section", "C"],
-  // ["What is the correct syntax for referring to an external script called 'xxx.js'?", "script src='xxx.js'", "script href='xxx.js'", "script name ='xxx.js'", "A"],
-  // ["How do you write 'Hello World' in an alert box?", "alert('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "A"],
-  // ["How do you create a function in JavaScript?", "function:myFunction()", "function = myFunction()", "function myFunction()", "C"],
+  ["Where is the correct place to insert a JavaScript??", "The head section", "Both the head section and the body section", "The body section", "B"],
+  ["What is the correct syntax for referring to an external script called 'xxx.js'?", "script src='xxx.js'", "script href='xxx.js'", "script name ='xxx.js'", "A"],
+  ["How do you write 'Hello World' in an alert box?", "alert('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "A"],
+  ["How do you create a function in JavaScript?", "function:myFunction()", "function = myFunction()", "function myFunction()", "C"],
   // ["How do you call a function named 'myFunction'?", "myFunction()", "call myFunction()", "call function myFunction()", "A"],
   // ["How to write an IF statement in JavaScript?", "if i = 5 then", "if (i==5)", "if i = 5 ", "B"],
   // ["What is the correct way to write a JavaScript array?", "var colors ='red', 'green', 'blue'", "var colors = ['red', 'green', 'blue']", "var colors = 1:'red', 2:'green', 3:'blue'", "B"],
@@ -31,10 +28,12 @@ function get(x) {
 function renderQuestion() {
   test = get("test");
   if (qNum >= questions.length) {
-    test.innerHTML = "<h2>You got " + correct + " of " + questions.length + " questions correct</h2>";
-    get("test_status").innerHTML = "Test completed";
     // stops rest of renderQuestion function running when test is completed
-    return false;
+    recordScores();
+    test.innerHTML = "<h2>You got " + correct + " of " + questions.length + " questions correct.</h2>";
+    test.innerHTML = "<h2>You scored " + score + " out of 50.  The high score is " + highscore + ".</h2>";
+    get("test_status").innerHTML = "Test completed";
+    return;
   }
   get("test_status").innerHTML = "Question " + (qNum + 1) + " of " + questions.length;
   question = questions[qNum][0];
@@ -47,9 +46,32 @@ function renderQuestion() {
   test.innerHTML += "<input type='radio' name='choices' value='B'> " + chB + "<br>";
   test.innerHTML += "<input type='radio' name='choices' value='C'> " + chC + "<br><br>";
   test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+  startTimer();
+}
+
+function startTimer() {
+  secondsRemaining = 5;
+  interval = setInterval(decrementTime, 1000);
+}
+
+function decrementTime() {
+  //alert if no time left and clear interval
+  secondsRemaining--;
+  // renderTime();
+  if (secondsRemaining === 0) {
+    stopTimer();
+    alert("Time's Up.  5 points deducted.  Next question.");
+    checkAnswer();
+  }
+}
+
+function stopTimer() {
+  // secondsRemaining = 0;
+  clearInterval(interval)
 }
 
 function checkAnswer() {
+  stopTimer();
   // use getElementsByName because we have an array which it will loop through
   choices = document.getElementsByName("choices");
   for (var i = 0; i < choices.length; i++) {
@@ -61,31 +83,29 @@ function checkAnswer() {
   if (choice == questions[qNum][4]) {
     //each time there is a correct answer this value increases
     correct++;
+    score = score + 5
   }
-  // changes position of which character user is on
+
+  // Updates the score
+  pointsDeducted = (5 - secondsRemaining);
+  score = score - pointsDeducted
+  console.log("Points Deducted: " + pointsDeducted)
+
+  // increments the question number
   qNum++;
+
   // then the renderQuestion function runs again to go to next question
   renderQuestion();
 }
-function gameOver(){
-//records the time the quiz ends
-var end = Date.now();
-var elapsed = end - start; // elapsed time in milliseconds
-console.log(elapsed);
-console.log(highscore);
-}
 
-function recordScores (){
-  // Calculates the score from number correct and time elapsed.
-var score = (correct * 1000 - elapsed);
-
-// Sets the score to localstorage.
-localStorage.setItem("score", score);
-
-// Compares the highscore to the score in this game.
-if (highscore !== null) {
+function recordScores() {
+  // Compares the highscore to the number answered correct in this game.
+  // if(highscore !== null){
   if (score > highscore) {
+    highscore = score;
     localStorage.setItem("highscore", score);
+    localStorage.setItem("correct", correct);
   }
 }
-}
+
+renderQuestion();
